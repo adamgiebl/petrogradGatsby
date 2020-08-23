273c1f33978869121941840d72afbbf49081b763
Adam <adamgiebl@gmail.com>
2020-08-23 23:13:49 +0200

    product fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import "./index.sass";
+
+const ALL_COURSES = "https://kea-alt-del.dk/t5/api/productlist";
+const IMAGE_BASE = "https://kea-alt-del.dk/t5/site/imgs";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch(ALL_COURSES)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res[2]);
+        setCourses(res);
+      });
+  }, []);
+
+  const getImageSrc = (imgName) => {
+    return IMAGE_BASE + "/small/" + imgName + "-sm.jpg";
+  };
+
+  return (
+    <div>
+      <header className="header">
+        <h1>Petrograd</h1>
+      </header>
+      <main className="items">
+        {courses &&
+          courses.map((course) => (
+            <article key={course.id}>
+              <img src={getImageSrc(course.image)} alt="" /> {course.name}
+            </article>
+          ))}
+      </main>
+    </div>
+  );
 }
+
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+//import altonTheme from "typography-theme-alton";
+import fairy from "typography-theme-fairy-gates";
+
+const typography = new Typography(fairy);
+
+export default typography;
