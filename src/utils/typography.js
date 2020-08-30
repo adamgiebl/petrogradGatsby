734ec428e86971e492d12f0f287444838f677bc0
Adam Giebl <adamgiebl@gmail.com>
2020-08-30 22:11:18 +0200

    progress

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-//import altonTheme from "typography-theme-alton";
-import fairy from "typography-theme-fairy-gates";
-
-const typography = new Typography(fairy);
-
-export default typography;
